2696e0706bea38d8864603f5fecb41b69b54e855
Sonu2396 <42073269+Sonu2396@users.noreply.github.com>
2024-01-16 12:52:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+
 const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
+
+// Enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
+
 
 const PORT = process.env.PORT || 4000;
 
