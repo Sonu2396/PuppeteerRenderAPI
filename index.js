471c40101a012b55efe95e66f73f3f0f37fa6a6e
Sonu2396 <42073269+Sonu2396@users.noreply.github.com>
2024-02-28 10:54:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,15 @@ app.use(function (req, res, next) {
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-
   const param1 = req.query.param1;
-  const param2 = req.query.param2;
-    
+  const param2 = req.query.param2;    
   scrapeLogic(res,param1, param2);
 });
+
+
+//app.get("/scrape", (req, res) => {
+//  scrapeLogic(res);
+//});
 
 app.get("/", (req, res) => {
   res.send("Render Puppeteer server is up and running!");
