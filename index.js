3393c000904391139d3e9c40797d3f397b4831b0
Sonu2396 <42073269+Sonu2396@users.noreply.github.com>
2024-03-02 10:52:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(function (req, res, next) {
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", (req, res) => {
-  const param1 = req.query.param1;
-  const param2 = req.query.param2;    
+  //const param1 = req.query.param1;
+  //const param2 = req.query.param2;    
   scrapeLogic(res);
 });
 
